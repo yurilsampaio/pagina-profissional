14ce199588d96e4f5c9fab601dd87e00ca7a77de
Yuri Sampaio <yurillsampaio@gmail.com>
2025-07-30 20:53:25 -0300

    Updates

---
+++ _next/static/chunks/app/layout-a7914e32e12e1fcd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1303:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,9840,23)),Promise.resolve().then(l.t.bind(l,9324,23))},9324:()=>{},9840:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_0cfb18"}}},e=>{var s=s=>e(e.s=s);e.O(0,[385,441,684,358],()=>s(1303)),_N_E=e.O()}]);
